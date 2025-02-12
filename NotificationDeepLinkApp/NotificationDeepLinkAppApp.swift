a156a66926281bb1d0a627367fb6485c567a2c06
Mohit Sharma <mohitksharmaa@yahoo.com>
2025-02-12 11:08:17 +0530

    Initial Commit

---
+++ NotificationDeepLinkApp/NotificationDeepLinkAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotificationDeepLinkAppApp.swift
+//  NotificationDeepLinkApp
+//
+//  Created by Mohit Sharma on 12/02/25.
+//
+
+import SwiftUI
+
+@main
+struct NotificationDeepLinkAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
