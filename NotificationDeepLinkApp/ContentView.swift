7dd7fe4c842ffe41bb77a728adc44adf2e20b6c0
Mohit Sharma <mohitksharmaa@yahoo.com>
2025-02-12 14:45:28 +0530

    Module completed

--- NotificationDeepLinkApp/ContentView.swift
+++ NotificationDeepLinkApp/ContentView.swift
@@ -8,14 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(AppData.self) private var appData
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        @Bindable var appData = appData
+        NavigationStack(path: $appData.mainPageNavigationPath){
+            List{
+                NavigationLink("View 1", value: "View1")
+                NavigationLink("View 2", value: "View2")
+                NavigationLink("View 3", value: "View3")
+            }
+            .navigationTitle("Notification Deep Link")
+            .navigationDestination(for: String.self){ value in
+                Text("Hello from \(value) page.")
+                    .navigationTitle(value)
+            }
         }
-        .padding()
+        .task {
+            let _ = try? await UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound])
+        }
+
     }
 }
 
